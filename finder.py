8b3ab8d023868909ab8b47c5d1b30e1bf288e4f0
12300238 <prades.mathew@gmail.com>
2025-07-19 15:02:32 +0200

    vérificaton schéma légit V2

--- finder.py
+++ finder.py
@@ -28,33 +28,81 @@ def pasdouble(tab:List[int]):
 
 def legite(tab:List[int]):
   i = 0
+  précedent = []
   while i+1<len(tab):
-    if tab[i]==1 and not (tab[i+1] in [2,4,5]):
-      return False
-    elif tab[i]==2 and not (tab[i+1] in [1,4,5,6,3]):
-      return False
-    elif tab[i]==3 and not (tab[i+1] in [2,5,6]):
-      return False
-    elif tab[i]==4 and not (tab[i+1] in [1,2,5,8,7]):
-      return False
-    elif tab[i]==5 and not (tab[i+1] in [1,2,3,4,6,8,7,9]):
-      return False
-    elif tab[i]==6 and not (tab[i+1] in [2,3,5,8,9]):
-      return False
-    elif tab[i]==7 and not (tab[i+1] in [4,5,8]):
-      return False
-    elif tab[i]==8 and not (tab[i+1] in [7,4,5,6,9]):
-      return False
-    elif tab[i]==9 and not (tab[i+1] in [8,5,6]):
-      return False
+    if tab[i]==1:
+      if not 2 in précedent and 3 == tab[i+1]:
+        return False
+      elif not 4 in précedent and 7 == tab[i+1]:
+        return False
+      elif not 5 in précedent and 9 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [2,4,5,8,6]):
+        return False
+      
+    elif tab[i]==2:
+      if not 5 in précedent and 8 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [1,3,4,5,6,7,9]):
+        return False
+      
+    elif tab[i]==3:
+      if not 2 in précedent and 1 == tab[i+1]:
+        return False
+      elif not 6 in précedent and 9 == tab[i+1]:
+        return False
+      elif not 5 in précedent and 7 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [2,4,5,8,6]):
+        return False
+      
+    elif tab[i]==4:
+      if not 5 in précedent and 6 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [1,2,3,5,7,8,9]):
+        return False
+      
+    elif tab[i]==6:
+      if not 5 in précedent and 4 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [1,2,3,4,7,8,9]):
+        return False
+      
+    elif tab[i]==7:
+      if not 4 in précedent and 1 == tab[i+1]:
+        return False
+      elif not 8 in précedent and 9 == tab[i+1]:
+        return False
+      elif not 5 in précedent and 3 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [2,4,5,8,6]):
+        return False
+      
+    elif tab[i]==8:
+      if not 5 in précedent and 2 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [1,3,4,5,6,7,9]):
+        return False
+      
+    elif tab[i]==9:
+      if not 6 in précedent and 3 == tab[i+1]:
+        return False
+      elif not 5 in précedent and 1 == tab[i+1]:
+        return False
+      elif not 8 in précedent and 7 == tab[i+1]:
+        return False
+      elif not (tab[i+1] in [2,4,5,8,6]):
+        return False
+    
+    précedent.append(tab[i])
     i+=1
   return True
         
 
 e=0
-while e<=100:
+while e<=1000:
   res=incrémenter(actuelle)
-  if legite(res):
+  if pasdouble(res) and legite(res):
     print(res)
   actuelle = res
   e+=1
