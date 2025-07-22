8c736ef569a2f0fda0decc80742c40fa57c5431b
12300238 <prades.mathew@gmail.com>
2025-07-22 14:59:44 +0200

    brut_force v0.1

---
+++ brut_force.py
@@ -0,0 +1,18 @@
+from time import sleep
+from calcule_possibilité.finder import pasdouble, incrémenter, legite
+from sourie.execution_possibilite import click_and_move
+
+actuelle = [1]
+
+#liste traitée avtuellement
+res = []
+
+#liste de toutes les possibilités
+final = []
+
+while len(res) <= 2:
+  res=incrémenter(actuelle)
+  if pasdouble(res) and legite(res):
+    click_and_move(res)
+    sleep(0.5)
+  actuelle = res
